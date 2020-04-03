8faadedd6bd283db8dfaac2491985a0af0a8964b
Arafat Najjashi <arafath.moghul@gmail.com>
2020-04-03 20:13:41 +0530

    added recipe.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe'
 import * as searchView from './views/searchView';
 import { elements,renderLoader, clearLoader } from './views/base';
 
@@ -11,6 +12,9 @@ import { elements,renderLoader, clearLoader } from './views/base';
 
 const state = {};
 
+/*
+SEARCH CONTROLLER
+*/
 const controlSearch = async () => {
 
     //Get the query
@@ -49,3 +53,11 @@ elements.searchResultPages.addEventListener('click', e=>{
         searchView.renderResults(state.search.result,goToPage);
     }
 });
+
+
+/*
+RECIPE CONTROLLER
+*/
+
+const r = new Recipe(47746);
+r.getRecipe();
