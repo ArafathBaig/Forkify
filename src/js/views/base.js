5604568a8aeb85c8db4acfc1311a498f3ea806bd
Arafat Najjashi <arafath.moghul@gmail.com>
2020-07-11 21:41:29 +0530

    listview

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -4,7 +4,8 @@ const elements ={
     serachRes : document.querySelector('.results'),
     searchResultList: document.querySelector('.results__list'),
     searchResPages: document.querySelector('.results__pages'),
-    recipe: document.querySelector('.recipe')
+    recipe: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list')
 };
 
 const elementStrings ={
